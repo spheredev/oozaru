a09b702f7c844f75e4da74bdae85cbfc2fead924
Bruce Pascoe <fatcerberus@icloud.com>
2018-10-09 01:36:28 -0400

    Convert codebase to ES modules
    I also rewrote Dispatch while I was at it.  I'm not entirely happy with
    the result, though, and I'm thinking it could do with some refactoring
    to whip it into shape.

--- oozaru/from.js
+++ oozaru/from.js
@@ -1,62 +1,84 @@
 /**
- *  miniRT "from" CommonJS module
- *  (c) 2016 Fat Cerberus
+ *  Oozaru JavaScript game engine
+ *  Copyright (c) 2016-2018, Fat Cerberus
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oozaru nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
 **/
 
-'use strict';
-module.exports = from;
-
-const assert = require('assert');
-const random = require('random');
-
-function from(target/*, ...*/)
+export default
+function from(...targets)
 {
 	// for multiple targets, query against the list of targets and unroll with
 	// .from().
-	if (arguments.length > 1)
-		return from(arguments).from();
+	if (targets.length > 1)
+		return from(targets).from();
 
-	target = Object(target);
+	let target = Object(targets[0]);
 	if (typeof target.length === 'number')
 		return fromArray(target);
+	else if (typeof target[Symbol.iterator] === 'function' || typeof target.next === 'function')
+		return fromIterable(target);
 	else
 		return fromObject(target);
-};
+}
 
-from.Array = fromArray;
+from.array = fromArray;
 function fromArray(target)
 {
 	target = Object(target);
 	if (typeof target.length !== 'number')
 		throw new TypeError("object with 'length' required");
-	var itemSource = new ArraySource(target);
+	let itemSource = new ArraySource(target);
 	return new FromQuery(itemSource);
 }
 
-from.Object = fromObject;
+from.iterable = fromIterable;
+function fromIterable(target)
+{
+	target = Object(target);
+	if (typeof target[Symbol.iterator] !== 'function' && typeof target.next !== 'function')
+		throw new TypeError("object is not iterable");
+	let itemSource = new IterableSource(target);
+	return new FromQuery(itemSource);
+}
+
+from.object = fromObject;
 function fromObject(target)
 {
-	var itemSource = new ObjectSource(Object(target));
+	let itemSource = new ObjectSource(Object(target));
 	return new FromQuery(itemSource);
 }
-
-const PK =
-{
-	ItemSource: Symbol('itemSource'),
-};
 
 function MAKEPOINT(sourceType, op)
 {
-	// this is some crazy witchcraft, don't try this at home!
-	const makeArray = Function.prototype.call.bind(Array.prototype.slice);
-
-	return function(/*...*/)
-	{
-		// some more crazy witchcraft to emulate ES6 Reflect.construct().
-		var source = this[PK.ItemSource];
-		var boundArgs = [ null, source ].concat(makeArray(arguments));
-		var constructor = Function.prototype.bind.apply(sourceType, boundArgs);
-		var newSource = new constructor();
+	return function (...args) {
+		let thisSource = this.itemSource;
+		let newSource = new sourceType(thisSource, ...args);
 
 		// if it's a terminal operator, run the query.  otherwise construct
 		// a new FromQuery.  this allows LINQ-style composition.
@@ -67,41 +89,35 @@ function MAKEPOINT(sourceType, op)
 
 function PROPDESC(flags, valueOrGetter, setter)
 {
-	var desc = {
+	let desc = {
 		writable:     flags.indexOf('w') !== -1,
 		enumerable:   flags.indexOf('e') !== -1,
 		configurable: flags.indexOf('c') !== -1,
 	};
-	if (flags.indexOf('a') === -1)
+	if (flags.indexOf('a') === -1) {
 		desc.value = valueOrGetter;
+	}
 	else {
 		desc.get = valueOrGetter;
 		desc.set = setter;
 	}
 	return desc;
-};
+}
 
 function FromQuery(source)
 {
-	this[PK.ItemSource] = source;
+	this.itemSource = source;
 }
 
-Object.defineProperties(FromQuery.prototype,
-{
+Object.defineProperties(FromQuery.prototype, {
 	[Symbol.iterator]:
-	PROPDESC('wc', function enumerate()
+	PROPDESC('wc', function* enumerate(withKeys)
 	{
-		var source = this[PK.ItemSource];
+		let source = this.itemSource;
+		let item;
 		source.init();
-		return { next: next };
-
-		function next()
-		{
-			var item = source.next();
-			return item !== null
-				? { value: item, done: false }
-				: { done: true };
-		}
+		while ((item = source.next()))
+			yield withKeys ? item : item.v;
 	}),
 
 	// from() query operators.
@@ -120,22 +136,23 @@ Object.defineProperties(FromQuery.prototype,
 	from:       PROPDESC('wc', MAKEPOINT(FromSource)),
 	including:  PROPDESC('wc', MAKEPOINT(IncludeSource)),
 	last:       PROPDESC('wc', MAKEPOINT(FilterSource, lastOp)),
-	mapTo:      PROPDESC('wc', MAKEPOINT(MapSource)),
 	random:     PROPDESC('wc', MAKEPOINT(SampleSource(false))),
+	reduce:     PROPDESC('wc', MAKEPOINT(ReduceSource, firstOp)),
 	remove:     PROPDESC('wc', MAKEPOINT(FilterSource, removeOp)),
 	sample:     PROPDESC('wc', MAKEPOINT(SampleSource(true))),
-	select:     PROPDESC('wc', MAKEPOINT(MapSource, collectOp)),
+	select:     PROPDESC('wc', MAKEPOINT(MapSource)),
 	shuffle:    PROPDESC('wc', MAKEPOINT(ShuffledSource)),
 	skip:       PROPDESC('wc', MAKEPOINT(SkipSource)),
 	take:       PROPDESC('wc', MAKEPOINT(TakeSource)),
+	toArray:    PROPDESC('wc', MAKEPOINT(MapSource, collectOp)),
 	update:     PROPDESC('wc', MAKEPOINT(MapSource, updateOp)),
 	where:      PROPDESC('wc', MAKEPOINT(FilterSource)),
 });
 
 function ArraySource(target)
 {
-	var m_index;
-	var m_length;
+	let m_index;
+	let m_length;
 
 	this.init =
 	function init()
@@ -152,16 +169,43 @@ function ArraySource(target)
 		return {
 			v: target[m_index],
 			k: m_index++,
-			t: target
+			t: target,
 		};
-	}
+	};
+}
+
+function IterableSource(target)
+{
+	let m_iter;
+
+	this.init =
+	function init()
+	{
+		if (typeof target[Symbol.iterator] === 'function')
+			m_iter = target[Symbol.iterator]();
+		else
+			m_iter = target;
+	};
+
+	this.next =
+	function next()
+	{
+		let result = m_iter.next();
+		if (result.done)
+			return null;
+		return {
+			v: result.value,
+			k: null,
+			t: target,
+		};
+	};
 }
 
 function ObjectSource(target)
 {
-	var m_index;
-	var m_keys;
-	var m_length;
+	let m_index;
+	let m_keys;
+	let m_length;
 
 	this.init =
 	function init()
@@ -174,13 +218,13 @@ function ObjectSource(target)
 	this.next =
 	function next()
 	{
-		if (m_index >= m_length);
+		if (m_index >= m_length)
 			return null;
-		var key = m_keys[m_index++];
+		let key = m_keys[m_index++];
 		return {
 			v: target[key],
 			k: key,
-			t: target
+			t: target,
 		};
 	};
 }
@@ -196,7 +240,7 @@ function CallbackSource(source, callback)
 	this.next =
 	function next()
 	{
-		var item = source.next();
+		let item = source.next();
 		if (item !== null)
 			callback(item.v, item.k, item.t);
 		return item;
@@ -205,7 +249,7 @@ function CallbackSource(source, callback)
 
 function FilterSource(source, predicate)
 {
-	predicate = predicate || function(v) { return true; };
+	predicate = predicate || (() => true);
 
 	this.init =
 	function init()
@@ -216,8 +260,8 @@ function FilterSource(source, predicate)
 	this.next =
 	function next()
 	{
-		var item;
-		while (item = source.next()) {
+		let item;
+		while ((item = source.next())) {
 			if (predicate(item.v, item.k, item.t))
 				break;
 		}
@@ -227,9 +271,9 @@ function FilterSource(source, predicate)
 
 function FromSource(source, selector)
 {
-	var m_iterator;
-	var m_nextItem;
-	var m_selector = selector || function(v) { return v; };
+	let m_iterator;
+	let m_nextItem;
+	let m_selector = selector || (it => it);
 
 	this.init =
 	function init()
@@ -242,18 +286,19 @@ function FromSource(source, selector)
 	function next()
 	{
 		while (m_iterator === null) {
-			var item = source.next();
+			let item = source.next();
 			if (item !== null) {
-				var target = m_selector(item.v, item.k, item.t);
-				m_iterator = from(target)[Symbol.iterator]();
+				let target = m_selector(item.v, item.k, item.t);
+				m_iterator = from(target)[Symbol.iterator](true);
 				if ((m_nextItem = m_iterator.next()).done)
 					m_iterator = null;
 			}
-			else
+			else {
 				return null;
+			}
 		}
 
-		var item = m_nextItem.value;
+		let item = m_nextItem.value;
 		if ((m_nextItem = m_iterator.next()).done)
 			m_iterator = null;
 		return item;
@@ -271,9 +316,9 @@ function InSource(source, values)
 	this.next =
 	function next()
 	{
-		var item = source.next();
+		let item = source.next();
 		if (item !== null) {
-			for (var i = values.length - 1; i >= 0; --i) {
+			for (let i = values.length - 1; i >= 0; --i) {
 				if (item.v === values[i]) {
 					item.v = true;
 					return item;
@@ -285,24 +330,23 @@ function InSource(source, values)
 	};
 }
 
-function IncludeSource(source, target)
+function IncludeSource(source, ...targets)
 {
-	var m_iterator;
-	var m_targets = Array.prototype.slice.call(arguments, 1);
+	let m_iterator;
 
 	this.init =
 	function init()
 	{
 		source.init();
-		m_iterator = from(m_targets).from()[Symbol.iterator]();
+		m_iterator = from(targets).from()[Symbol.iterator](true);
 	};
 
 	this.next =
 	function next()
 	{
-		var item = source.next();
+		let item = source.next();
 		if (item === null) {
-			var result;
+			let result;
 			if (!(result = m_iterator.next()).done)
 				item = result.value;
 		}
@@ -321,7 +365,7 @@ function IsSource(source, value)
 	this.next =
 	function next()
 	{
-		var item = source.next();
+		let item = source.next();
 		if (item !== null)
 			item.v = item.v === value;
 		return item;
@@ -330,7 +374,7 @@ function IsSource(source, value)
 
 function MapSource(source, selector)
 {
-	selector = selector || function(v) { return v; };
+	selector = selector || (it => it);
 
 	this.init =
 	function init()
@@ -341,7 +385,7 @@ function MapSource(source, selector)
 	this.next =
 	function next()
 	{
-		var item = source.next();
+		let item = source.next();
 		if (item !== null)
 			item.v = selector(item.v, item.k, item.t);
 		return item;
@@ -356,38 +400,37 @@ function MapSource(source, selector)
 // bubble sort, but with eaty pigs.
 function OrderedSource(descending)
 {
-	return function(source, keySelector)
-	{
-		var m_index = 0;
-		var m_length;
-		var m_list = [];
+	return function (source, keySelector) {
+		let m_index = 0;
+		let m_length;
+		let m_list = [];
 
 		this.init =
 		function init()
 		{
-			var index;
-			var item;
-			var order = descending ? -1 : 1;
+			let index;
+			let item;
+			let order = descending ? -1 : 1;
 
 			// this is kind of ugly pulling all results in advance, but there's
 			// not much we can do about it here.  we could sort as we go, but
 			// since the rest of the query can't do anything until the results
 			// are fully sorted anyway, that doesn't buy us much.
 			source.init();
-			while (item = source.next()) {
+			while ((item = source.next())) {
 				index = m_list.length;
 				m_list[index] = {
 					index: index,  // to stabilize the sort
 					item:  item,
-					key:   keySelector(item.v, item.k, item.t)
+					key:   keySelector(item.v, item.k, item.t),
 				};
 			}
 
 			// Array#sort() is not guaranteed to be stable.  to stabilize it,
 			// we use the item's position in the input stream as a tiebreaker.
-			m_list.sort(function(a, b) {
+			m_list.sort((a, b) => {
 				return a.key < b.key ? -1 * order
-					: a.key > b.key ? 1 * order
+					: a.key > b.key ? +1 * order
 					: a.index - b.index;
 			});
 			m_index = 0;
@@ -405,21 +448,59 @@ function OrderedSource(descending)
 	};
 }
 
+function ReduceSource(source, reducer, initialValue)
+{
+	let m_sentValue = false;
+	let m_value = undefined;
+
+	this.init =
+	function init()
+	{
+		let item;
+
+		// as with the sorting operators, Fisher-Yates shuffle doesn't really
+		// lend itself to streaming so we just pull everything in advance.
+		source.init();
+		if (initialValue !== undefined) {
+			m_value = initialValue;
+		}
+		else {
+			if ((item = source.next()))
+				m_value = item.v;
+			else
+				return;
+		}
+		while ((item = source.next()))
+			m_value = reducer(m_value, item.v, item.k, item.t);
+	};
+
+	this.next =
+	function next()
+	{
+		if (!m_sentValue) {
+			m_sentValue = true;
+			return { v: m_value };
+		}
+		else {
+			return null;
+		}
+	};
+}
+
 function SampleSource(uniqueOnly)
 {
-	return function(source, count)
-	{
-		var m_count = Number(count);
-		var m_items = [];
-		var m_numSamples;
+	return function (source, count) {
+		let m_count = Number(count);
+		let m_items = [];
+		let m_numSamples;
 
 		this.init =
 		function init()
 		{
-			var item;
+			let item;
 
 			source.init();
-			while (item = source.next())
+			while ((item = source.next()))
 				m_items[m_items.length] = item;
 			m_numSamples = 0;
 		};
@@ -427,44 +508,45 @@ function SampleSource(uniqueOnly)
 		this.next =
 		function next()
 		{
-			var index;
-			var item;
+			let index;
+			let item;
 
 			if (m_numSamples++ < m_count) {
-				index = random.discrete(0, m_items.length - 1);
+				index = Math.floor(Math.random() * m_items.length);
 				item = m_items[index];
 				if (uniqueOnly)
 					m_items.splice(index, 1);
 				return item;
 			}
-			else
+			else {
 				return null;
+			}
 		};
 	};
 }
 
 function ShuffledSource(source)
 {
-	var m_index = 0;
-	var m_length;
-	var m_list = [];
+	let m_index = 0;
+	let m_length;
+	let m_list = [];
 
 	this.init =
 	function init()
 	{
-		var index;
-		var item;
-		var temp;
+		let index;
+		let item;
+		let temp;
 
 		// as with the sorting operators, Fisher-Yates shuffle doesn't really
 		// lend itself to streaming so we just pull everything in advance.
 		source.init();
-		while (item = source.next()) {
+		while ((item = source.next())) {
 			index = m_list.length;
 			m_list[index] = item;
 		}
-		for (var i = m_list.length - 1; i >= 1; --i) {
-			index = random.discrete(0, i);
+		for (let i = m_list.length - 1; i >= 1; --i) {
+			index = Math.floor(Math.random() * i);
 			temp = m_list[index];
 			m_list[index] = m_list[i];
 			m_list[i] = temp;
@@ -485,8 +567,8 @@ function ShuffledSource(source)
 
 function SkipSource(source, count)
 {
-	var m_count = Number(count);
-	var m_numSkipped;
+	let m_count = Number(count);
+	let m_numSkipped;
 
 	this.init =
 	function init()
@@ -498,7 +580,7 @@ function SkipSource(source, count)
 	this.next =
 	function next()
 	{
-		var item;
+		let item;
 
 		item = m_numSkipped++ >= m_count
 			? source.next() : null;
@@ -508,8 +590,8 @@ function SkipSource(source, count)
 
 function TakeSource(source, count)
 {
-	var m_count = Number(count);
-	var m_numTaken;
+	let m_count = Number(count);
+	let m_numTaken;
 
 	this.init =
 	function init()
@@ -521,7 +603,7 @@ function TakeSource(source, count)
 	this.next =
 	function next()
 	{
-		var item;
+		let item;
 
 		item = m_numTaken++ < m_count
 			? source.next() : null;
@@ -531,69 +613,68 @@ function TakeSource(source, count)
 
 function allOp(source)
 {
-	var item;
+	let item;
 
 	source.init();
-	while (item = source.next()) {
+	while ((item = source.next())) {
 		if (!item.v)
 			return false;
 	}
 	return true;
-};
+}
 
 function anyOp(source)
 {
-	var item;
+	let item;
 
 	source.init();
-	while (item = source.next()) {
-		if (!!item.v)
+	while ((item = source.next())) {
+		if (item.v)
 			return true;
 	}
 	return false;
-};
+}
 
 function collectOp(source)
 {
-	var collection = [];
-	var item;
+	let collection = [];
+	let item;
 
 	source.init();
-	while (item = source.next())
+	while ((item = source.next()))
 		collection[collection.length] = item.v;
 	return collection;
-};
+}
 
 function countOp(source)
 {
-	var numItems = 0;
+	let numItems = 0;
 
 	source.init();
 	while (source.next())
 		++numItems;
 	return numItems;
-};
+}
 
 function firstOp(source)
 {
-	var item;
+	let item;
 
 	source.init();
-	if (item = source.next())
+	if ((item = source.next()))
 		return item.v;
-};
+}
 
 function lastOp(source)
 {
-	var lastResult;
-	var item;
+	let lastResult;
+	let item;
 
 	source.init();
-	while (item = source.next()) {
+	while ((item = source.next()))
 		lastResult = item.v;
-	}
 	return lastResult;
-};
+}
 
 function nullOp(source)
 {
@@ -601,31 +682,31 @@ function nullOp(source)
 	while (source.next()) {
 		/* no-op */
 	}
-};
+}
 
 function removeOp(source)
 {
-	var item;
-	var splice = Array.prototype.splice;
-	var toRemove = [];
+	let item;
+	let splice = Array.prototype.splice;
+	let toRemove = [];
 
 	source.init();
-	while (item = source.next())
+	while ((item = source.next()))
 		toRemove[toRemove.length] = { k: item.k, t: item.t };
-	for (var i = toRemove.length - 1; i >= 0; --i) {
+	for (let i = toRemove.length - 1; i >= 0; --i) {
 		item = toRemove[i];
 		if (typeof item.t.length === 'number')
 			splice.call(item.t, item.k, 1);
 		else
-			delete item.t[item.k]
+			delete item.t[item.k];
 	}
-};
+}
 
 function updateOp(source)
 {
-	var item;
+	let item;
 
 	source.init();
-	while (item = source.next())
+	while ((item = source.next()))
 		item.t[item.k] = item.v;
-};
+}
