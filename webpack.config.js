78f09cbb43c178009f089c13a9ad11075edbdf6b
Bruce Pascoe <fatcerberus@icloud.com>
2019-06-01 00:42:44 -0400

    Tinker with webpack configuration
    Just trying to get a feel for how everything works.  So far webpack
    seems pretty simple.  Certainly a lot moreso than I expected.

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,53 @@
+/*
+ *  Oozaru JavaScript game engine
+ *  Copyright (c) 2015-2019, Fat Cerberus
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of miniSphere nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+**/
+
 const path = require('path');
 
 module.exports =
 {
-    /**
-     * @see https://webpack.js.org/configuration/devtool#devtool
-     */
-    devtool: false,
-    /**
-     * @see https://webpack.js.org/configuration/entry-context/#entry
-     */
+    mode: 'production',
+
     entry: './src/main.ts',
-    /**
-     * @see https://webpack.js.org/configuration/module/
-     */
     module: {
         rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
+            { test: /\.ts$/, use: 'ts-loader' },
         ]
     },
-    /**
-     * @see https://webpack.js.org/configuration/resolve/
-     */
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: [ '.ts' ],
     },
-    /**
-     * @see https://webpack.js.org/configuration/output/
-     */
+
+    devtool: 'source-map',
+
     output: {
         chunkFilename: 'oozaru-dl-[id].js',
         filename: 'oozaru.js',
@@ -38,18 +55,5 @@ module.exports =
         libraryTarget: 'var',
         path: __dirname,
         sourceMapFilename: '[file].map',
-    },
-    /**
-     * Polyfill or mock certain Node.js globals and modules for the browser.
-     * @see https://webpack.js.org/configuration/node/
-     */
-    node: {
-        clearImmediate: false,
-        crypto: 'empty',
-        fs: 'empty',
-        global: true,
-        module: false,
-        process: true,
-        setImmediate: false,
     },
 };
