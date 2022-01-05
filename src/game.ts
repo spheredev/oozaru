5701345493ec9517e0920140fa5bd5bdd9f24b01
Bruce Pascoe <fatcerberus@icloud.com>
2022-01-05 12:46:05 -0500

    Reorganize `web` directory
    Mostly just renaming folders for clarity and to make the layout a bit
    more self-documenting.

--- src/game.ts
+++ src/game.ts
@@ -128,7 +128,7 @@ class Game
 		}
 		else if (hops[0] === '#') {
 			hops.splice(0, 1);
-			return `assets/${hops.join('/')}`;
+			return `resources/${hops.join('/')}`;
 		}
 		else {
 			throw new RangeError(`Unsupported SphereFS prefix '${hops[0]}'`);
